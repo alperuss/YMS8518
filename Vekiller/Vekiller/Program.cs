226cf5a746e04a60f4559b0af4005064741e05dd
alper <alper.usss@gmail.com>
2020-02-08 16:28:45 +0300

    delegate

---
+++ Vekiller/Vekiller/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Vekiller
+{
+    class Program
+    {
+        public static string ConcattedString = string.Empty;
+        public static void Concat(string ilk,string son)
+        {
+            ConcattedString = ilk + son;
+           Console.WriteLine(ilk + son);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Action<string, string> testDelegate2 = Concat;
+            testDelegate2("x","y");
+
+            
+            Console.WriteLine("Singleton'dan gelen nesne: "+ ConcattedString);
+          
+            Console.ReadLine();
+            
+        }
+    }
+}
